d0909189a9dd7b2a5c00c4ea5a518a7a452b2996
UsmanAbid571 <149952584+UsmanAbid571@users.noreply.github.com>
2023-11-07 17:50:50 +0500

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,17 +1,31 @@
+
+
+
 print("What do you want?")
 first = input("Enter 'A' for arithmetic operations or 'B' for comparison of two numbers:  ").lower()
 if first == "a":
+
+    def add(n1, n2):
+            return n1 + n2
+    def sub(n1, n2):
+            return n1 - n2
+    def mul(n1, n2):
+            return n1 * n2
+    def div(n1, n2):
+            return n1 / n2
+
+    op = input("Enter the operation you want (+, -, *, /): ")
     n1 = float(input("Enter a number: "))
     n2 = float(input("Enter another number: "))
-    op = input("Enter the operator(+, -, *, /): ")
+
     if op == "+":
-        print(n1 + n2)
+        print(add(n1, n2))
     elif op == "-":
-        print(n1 - n2)
+        print(sub(n1, n2))
     elif op == "*":
-        print(n1 * n2)
+        print(mul(n1, n2))
     elif op == "/":
-        print(n1 / n2)
+        print(div(n1, n2))
     else:
         print("You entered the wrong operator")
 
