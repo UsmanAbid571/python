9e7caa690a97ae8e20131564d2c55ed8f2fff200
UsmanAbid571 <149952584+UsmanAbid571@users.noreply.github.com>
2023-11-07 16:18:33 +0500

    Add files via upload

--- dictionaries.py
+++ dictionaries.py
@@ -1,15 +1,20 @@
-month_names{
-    Jan : January,
-    feb : February,
-    mar : March,
-    apr : April,
-    may : May,
-    jun : June,
-    jul : July,
-    aug : August,
-    sep : September,
-    oct : October,
-    nov : November,
-    dec : December,
+
+input = input("Enter a month name in short: ")
+
+
+month_names = {
+    "jan" : "January",
+    "feb" : "February",
+    "mar" : "March",
+    "apr" : "April",
+    "may" : "May",
+    "jun" : "June",
+    "jul" : "July",
+    "aug" : "August",
+    "sep" : "September",
+    "oct" : "October",
+    "nov" : "November",
+    "dec" : "December",
 
 }
+print(month_names[input])
